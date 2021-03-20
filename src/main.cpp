bc727b0fa701970e7b9fd012edb81ca4e7a2d1f8
Jaagup <jaagup.averin@gmail.com>
2021-03-20 14:49:27 +0200

    prototyping

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,39 @@
 #include <iostream>
+#include <string>
 
-int main(int, char**) {
-    std::cout << "Hello, world!\n";
+enum class Result
+{
+    Continue,
+    ShouldExit,
+};
+
+Result run(std::string input)
+{
+    std::cout << input;
+    return Result::ShouldExit;
+}
+
+int main(int argc, char** argv)
+{
+    if (argc == 1) // Interactive mode:
+    {
+        std::cout << '>';
+        std::string input;
+        while (std::getline(std::cin, input))
+        {
+            auto result = run(input);
+            if (result == Result::ShouldExit)
+                break;
+        }
+    }
+    else // Input as arguments:
+    {
+        std::string input;
+        for (int i = 1; i != argc; ++i)
+        {
+            input += argv[i];
+            input += ' ';
+        }
+        run(input);
+    }
 }
